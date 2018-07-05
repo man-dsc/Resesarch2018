07b756c19001da8acd26b18ab3b29f1f22f47ad5
Manjeet Dev <manjeet.dev1@cs.ucalgary.ca>
2018-07-05 10:50:59 -0600

    Fixing git issues

--- MAPFEAT/MAPFEAT/server.py
+++ MAPFEAT/MAPFEAT/server.py
@@ -12,14 +12,23 @@ import topicModeling
 ''' Import Flask Backend '''
 from flask import *
 from werkzeug.utils import secure_filename # blocks malicious files disguised as csv
-import os
+import os, csv
 import random
 import shutil
 import requests
 import tablib
 
+from flask import Flask, render_template, request
+from wtforms import Field, TextField, widgets, SelectMultipleField, Form
+from wtforms.widgets import TextInput, html_params
+
+
+
+
+
 ''' Instantiation of Flask class and environment variables '''
 app = Flask(__name__)
+app.config.from_object(__name__)
 
 UPLOAD_FOLDER = os.path.dirname(os.path.realpath(__file__)) + '/data/'
 OUTPUT_FOLDER = os.path.dirname(os.path.realpath(__file__)) + '/output/'
@@ -27,6 +36,43 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['OUTPUT_FOLDER'] = OUTPUT_FOLDER
 ALLOWED_EXTENSIONS = set(['csv'])
 
+'''---------------------------------------------------------------'''
+
+
+
+
+
+
+''' Instantiation of class check boxes '''
+
+class MultiCheckboxField(SelectMultipleField):
+    widget = widgets.ListWidget(prefix_label=False)
+    option_widget = widgets.CheckboxInput()
+
+class SimpleForm(Form):
+    string_of_files = ['one\r\ntwo\r\nthree\r\n']
+    list_of_files = string_of_files[0].split()
+    # create a list of value/description tuples
+    files = [(x, x) for x in list_of_files]
+    example = MultiCheckboxField('Label', choices=files)
+
+
+class parameters_2:
+    def __init__(self, var1, var2, var3, var4):
+        self.keyword1 = var1
+        self.keyowrd2 = var2
+        self.keyword3 = var3
+        self.keyword4 = var4
+
+parameters_2 = parameters_2(-1, -1, -1, -1)
+
+
+
+
+
+
+
+'''---------------------------------------------------------------'''
 ''' Instantiation of class for environment vars '''
 class parameters:
     def __init__(self, var1, var2, var3, var4):
@@ -97,6 +143,124 @@ def inputs():
             return render_template('redirect.html')
     return render_template('inputs.html')
 
+'''-----------------------------------------------------------'''
+
+
+
+
+
+''' new page search '''
+@app.route('/screen1', methods = ['GET', 'POST'])
+def screen1():
+    formData = request.values if request.method == "GET" else request.values
+    if request.method == 'POST':
+        
+  
+        text1 = request.form['t_1']
+        text2 = request.form['t_2']
+        text3 = request.form['t_3']
+        text4 = request.form['t_4']
+        print([text1, text2, text3, text4])
+        try:
+            print('1')
+            text1, text2, text3, text4 = str(text1), str(text2), str(text3), str(text4)
+            print('2')
+            parameters_2.keyword1 = text1
+            parameters_2.keyword2 = text2
+            parameters_2.keyword3 = text3
+            parameters_2.keyword4 = text4
+            print(text1, text2, text3, text4)
+            print('')
+            print('')
+            print('')
+            print('Form Data')
+            print(formData)
+            print('')
+            print('')
+            print('')
+            print('')
+            response = "Form Contents <pre>%s</pre>" % "<br/>\n".join(["%s:%s" % item for item in formData.items(multi=True)] )
+            print(formData)
+            print(response)
+            checks = ''
+            if 'apple' in response:
+                checks += '1'
+            if 'google' in response:
+                checks += '2'
+            print('')
+            print(checks)
+            data = [checks, text1, text2, text3, text4]
+            
+            '''with open('Inputcsv.csv', 'wb') as fin:
+                writer = csv.writer(fin)
+                for row in data:
+                    writer.writerow(row)
+            '''
+           
+            
+            
+            
+            """form = SimpleForm()
+            print('next s')
+            print(form)
+            if form.validate_on_submit():
+                print('444444444444')
+                print form.example.data
+                print('55555555555')
+            else:
+                print('3333333333')
+                print form.errors"""
+
+
+            return redirect(url_for('loading')) 
+        except Exception as e:
+            print(e)
+            return render_template('about.html')
+    return render_template('screen1.html')
+
+
+'''-----------------------------------------------------------'''
+
+'''intermidiate page'''
+@app.route('/loading', methods = ['GET', 'POST'])
+def loading():
+    if request.method == 'GET':
+        '''Callfunction(data)
+        apps = []
+        devs = []
+        with open('Outputcsv.csv', 'r') as fout:
+            reader = csv.reader(fout)
+            for row in csvreader:
+                apps.append((row[0], row[1]))
+                #devs.append(row[1])
+            
+            '''
+        return render_template('results_screen1.html')
+        '''return redirect(url_for('results_screen1'))'''
+    return render_template('loading.html')
+
+'''-------------------------------------------------------------'''
+
+''' Results for screen 1 Page '''
+@app.route('/results_screen1', methods = ['GET', 'POST'])
+def results_screen1():
+    if request.method == 'GET':
+        for app, dev in apps:
+            print (app, dev)
+    
+    return render_template('results_screen1.htm')
+
+
+
+
+
+
+
+
+
+
+'''-------------------------------------------------------------'''
+
 ''' Results Page '''
 @app.route('/results', methods = ['GET', 'POST'])
 def results():
@@ -117,7 +281,8 @@ def results():
         
         # Process Tweets
         processTweets.process()
-        
+       
+        render_template('inputs.html')
         # Attain classification results - k fold validation results
         results = classifyTweets.classify()
         accuracy = results[0]
@@ -125,11 +290,15 @@ def results():
         recall = results[2]
         F1 = results[3]
         stdev = results[4]
-    
+   
+        render_template('inputs.html')
         topicModeling.extractTopics(wordsPerTopic, numTopics)
+   
         searchAppStore.search(appLimit)
+        
     
         extractFeatures.extract()
+      
         finalizeFeatures.finalize(sharedBetween)
         
         return render_template('results.html', accuracy = accuracy, \
@@ -139,6 +308,7 @@ def results():
 
 def permit(filename):
     return'.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS 
+    
 
 ''' Error Page '''
 @app.route('/error', methods = ['GET', 'POST'])
